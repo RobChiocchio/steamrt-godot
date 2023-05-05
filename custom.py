4b9e2eb10f4cbf7bcca0a0b4b4b9ad130e1b2c9f
Rob Chiocchio <RobethX@users.noreply.github.com>
2023-05-05 19:42:22 -0400

    disable_3d cannot be used for editor builds

--- custom.py
+++ custom.py
@@ -1,7 +1,7 @@
 # Generated using https://godot-build-options-generator.github.io
 
 extra_suffix = "boss_arena"
-disable_3d = "yes"
+#disable_3d = "yes"
 module_mobile_vr_enabled = "no"
 module_openxr_enabled = "no"
 module_webxr_enabled = "no"
