c6eecfbca155677a5a5d411927d52feb2fc1976f
Rob Chiocchio <RobethX@users.noreply.github.com>
2023-05-06 00:08:28 -0400

    Remove extra suffix

--- custom.py
+++ custom.py
@@ -1,6 +1,6 @@
 # Generated using https://godot-build-options-generator.github.io
 
-extra_suffix = "boss_arena"
+#extra_suffix = "boss_arena"
 #disable_3d = "yes"
 module_mobile_vr_enabled = "no"
 module_openxr_enabled = "no"
